2b2f9822c92584f6185ab8173984d8e3699cc439
zaenarifputra <zaenarifputraainurdin@gmail.com>
2022-01-19 11:45:56 +0700

    Modul 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Putra | About</title>
-</head>
-<body>
+      @extends('layouts.main')
 
-    <center><h1> Halaman About </h1>
-    <h3> <?php echo $nama; ?> </h3>
-    <p> <?php echo $email; ?> </p>
-    <img src="images/<?php echo $gambar; ?>" alt="<?php echo $nama; ?>" width="200px">
-    </center>
-    <script src="js/script.js"></script>
-</body>
-</html>
+      @section('container')
+      <h1> Halaman About </h1>
+      <h3> {{ $name }}</h3>
+      <p> {{ $email }}</p>
+      <img src="images/{{ $gambar }}" alt="{{ $name }}" widht="200px">
+      @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function (){
-    return view('home');
+Route::get('/', function () {
+    return view('home' , [
+        "title" => "Home"
+    ]);
 });
 
-Route::get('/about', function (){
-    return view('about', [
-        "nama" => "Zaenarif Putra 'Ainurdin",
+Route::get('/about', function () {
+    return view ('about', [
+        "title" => "About",
+        "name" => "Zaenarif Putra",
         "email" => "putrazaenarif@gmail.com",
         "gambar" => "putra.png"
     ]);
 });
 
-Route::get('/gallery', function (){
-    return ('gallery');
+Route::get('/gallery', function () {
+    return view ('gallery', [
+        "title" => "Gallery"
+    ]);
 });
